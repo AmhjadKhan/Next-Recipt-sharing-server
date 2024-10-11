b0d789065bfc5c7c61b9cfda94ea5b7f16118482
Amhjadkhan <amjadpogramer@gmail.com>
2024-10-11 19:23:12 +0400

    this is user  model design commit

---
+++ src/app/modules/User/user.route.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { UserControllers } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { userValidation } from './user.validation';
+import { multerUpload } from '../../config/multer.config';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
+
+const router = express.Router();
+
+router.post(
+  '/signup',
+  validateRequest(userValidation.createUserValidationSchema),
+  UserControllers.createUser,
+);
+
+router.patch(
+  `/user/:userId`,
+  multerUpload.single('image'),
+  UserControllers.updateUser,
+);
+
+router.post(
+  '/user/follow/:userId',
+  auth(USER_ROLE.admin, USER_ROLE.user),
+  UserControllers.followUser,
+);
+
+router.get(
+  '/user/following/:id',
+  UserControllers.getAllUserFollowers,
+);
+
+export const UserRoutes = router;
